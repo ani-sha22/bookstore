86949242b5604128f361837f0ee50de0ce6667e8
NeviLInit <antarikshkalbande27@gmail.com>
2023-04-09 21:42:43 +0530

    GoogleSignIn

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,3 +66,5 @@ const google_login = async (req, res) => {
 }
 
 module.exports = { signupUser, loginUser, google_login, google_signup }
+
+
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -105,3 +105,5 @@ userSchema.statics.login = async function(email, password) {
   }
 
 module.exports = mongoose.model('User', userSchema)
+
+
