5eb72aa79e607936eb3611276b58f0cf930b5421
Nehel Khanna <nehel.khanna20@st.niituniversity.in>
2023-04-09 22:04:29 +0530

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
+
+// pages & components
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignupBuyer from './pages/SignupBuyer'
+import SignupSeller from './pages/SignupSeller'
+import Navbar from './components/Navbar'
+import Dashboard from "./pages/Dashboard"
 
 function App() {
+  const { user } = useAuthContext()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route 
+              path="/"
+              element={<Home />}
+            />
+            <Route 
+              path="/login" 
+              element={user ? <Navigate to="/dashboard" /> : <Login />} 
+            />
+            <Route 
+              path="/signup-buyer" 
+              element={user ? <Navigate to="/dashboard" /> : <SignupBuyer />} 
+            />
+            <Route 
+              path="/signup-seller" 
+              element={user ? <Navigate to="/dashboard" /> : <SignupSeller />} 
+            />
+            <Route 
+              path="/dashboard" 
+              element={user ? <Dashboard /> : <Navigate to="/login" />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
